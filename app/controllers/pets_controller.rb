2edc41c1266951bec54bab78ba2996c0f560dfb1
your_github_username <valentinaleonh8@gmail.com>
2024-10-29 08:52:44 -0600

    Code basic controller actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,48 @@
 class PetsController < ApplicationController
+  def index
+    @pets = Pet.where(user: current_user)
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_path
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :pet_type, :breed, :weight, :height, :color, :birthday_date, :gender, :user_id)
+  end
 end
