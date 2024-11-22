2debc997ec4def76cb9d1486c754206b258cd19d
your_github_username <valentinaleonh8@gmail.com>
2024-11-21 21:37:54 -0600

    Add Data tests for veterinaries in Index and basic templates

--- app/controllers/veterinaries_controller.rb
+++ app/controllers/veterinaries_controller.rb
@@ -1,2 +1,37 @@
 class VeterinariesController < ApplicationController
+  def index
+    @veterinaries = Veterinary.all
+  end
+
+  def show
+    @veterinary = Veterinary.find(params[:id])
+  end
+
+  def new
+    @veterinary = Veterinary.new
+  end
+
+  def create
+    @veterinary = Veterinary.new(veterinary_params)
+    if @veterinary.save
+      redirect_to veterinaries_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def veterinary_params
+    params.require(:veterinary).permit(:specialization, :dni, :user_id)
+  end
 end
